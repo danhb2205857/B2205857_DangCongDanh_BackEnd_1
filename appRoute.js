05b82d1c150ac80cbcaf6180b8986db402472f93
danhtam1168 <danhtam1168@gmail.com>
2025-06-10 16:00:34 +0700

    them asyncHandle

--- appRoute.js
+++ appRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
 const router = express.Router()
 
-//import controller
+//import controllers
 import * as contactController from './controller/contactController'
 
+//import middlewares
+import asyncHandler from "./middlewares/asyncHandler";
+
 export function appRoute(app) {
-    router.get('/home', contactController.find)
+    router.get('/home', asyncHandler(contactController.find))
 
     app.use('/api/', router);
 }
