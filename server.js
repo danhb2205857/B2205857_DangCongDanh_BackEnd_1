a903ec08736dde0a30937d6a46781f38f726c967
danhtam1168 <danhtam1168@gmail.com>
2025-06-10 15:33:51 +0700

    cai dat babel

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express')
+import express from "express"
 const app = express()
-const port = 3000
+
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
